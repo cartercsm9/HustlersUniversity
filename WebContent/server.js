72528fcd3ff926e75fa5441fd62aa00cf01a2f45
cartercsm9 <cartercsm9@gmail.com>
2024-04-13 05:03:38 -0700

    timeout fix

--- WebContent/server.js
+++ WebContent/server.js
@@ -5,18 +5,17 @@ const db = require('./database');
 const initialDelay = 5000; // Delay in milliseconds (5000 ms = 5 seconds)
 console.log('Waiting 5 seconds before attempting to connect...');
 setTimeout(() => {
+    const port = 3001; // Port to listen on
+
+    db.on('connected', () => {
+        const server = http.createServer(app);
+        server.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        });
+    });
+
+    db.on('error', (err) => {
+        console.error('Failed to connect to the database:', err);
+    });
 }, initialDelay);
 
-
-const port = 3001; // Port to listen on
-
-db.on('connected', () => {
-    const server = http.createServer(app);
-    server.listen(port, () => {
-        console.log(`Server is listening on port ${port}`);
-    });
-});
-
-db.on('error', (err) => {
-    console.error('Failed to connect to the database:', err);
-});
